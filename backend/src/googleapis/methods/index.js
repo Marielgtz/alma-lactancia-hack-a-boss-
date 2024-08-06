d3ea6437809881f5679faf862fe91e2fd3d56dc7
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-08-06 21:37:30 +0200

    Crear métodos para añadir, modificar, listar, obtener (uno concreto) y eliminar eventos del calendario

--- backend/src/googleapis/methods/index.js
+++ backend/src/googleapis/methods/index.js
@@ -1,7 +1,23 @@
-import getValues from './getValues'
-import enterValue from './enterValue'
-import deleteRow from './deleteRow'
-import getSheetId from './getSheetId'
-import insertRow from './insertRow'
+import getValues from './getValues.js'
+import enterValue from './enterValue.js'
+import deleteRow from './deleteRow.js'
+import getSheetId from './getSheetId.js'
+import insertRow from './insertRow.js'
+import addEvent from './calendar/addEvent.js'
+import deleteEvent from './calendar/deleteEvent.js'
+import updateEvent from './calendar/updateEvent.js'
+import getEvent from './calendar/getEvent.js'
+import listEvents from './calendar/listEvents.js'
 
-export { getValues, enterValue, deleteRow, getSheetId, insertRow }
+export {
+    getValues,
+    enterValue,
+    deleteRow,
+    getSheetId,
+    insertRow,
+    addEvent,
+    deleteEvent,
+    updateEvent,
+    getEvent,
+    listEvents,
+}
