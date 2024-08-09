ba89231ec0ee952037554927838ff901a363090b
Alba Fresnillo Arévalo <fresnilloarevaloalba@gmail.com>
2024-08-09 10:53:03 +0200

    añadido formulario contacto y ruta para que al clickar en contacto en inicio te redirija a la página

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import logo from "../images/logo-alma.png";
+
+const Header = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleSubMenu = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header>
+      <nav className="navbar">
+        <img src={logo} alt="Logo Alma" className="logo" />
+        <div className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </div>
+        <ul className={`menu ${menuOpen ? "active" : ""}`}>
+          <li className="menu-item">
+            <a href="#">Inicio</a>
+          </li>
+          <li className="menu-item">
+            <a href="#">¿Quiénes somos?
+            </a>
+          </li>
+          <li className="menu-item">
+            <a href="#">Biblioteca</a>
+          </li>
+          <li className="menu-item">
+            <a href="#" onClick={() => toggleSubMenu(1)}>
+              Actividades
+            </a>
+            <ul className={`submenu ${activeIndex === 1 ? "active" : ""}`}>
+              <li>
+                <a href="#">Próximas Inscripciones</a>
+              </li>
+              <li>
+                <a href="#">Histórico</a>
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item">
+            <Link to="/contacto" onClick={toggleMenu}>Contacto</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
