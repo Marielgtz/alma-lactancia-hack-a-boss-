35f2834006c239ec49610cf0bfcf35984a7f7afa
Alba Fresnillo Arévalo <144828195+AlbaFresnillo@users.noreply.github.com>
2024-08-14 12:01:29 +0200

    Delete backend directory

--- backend/src/controllers/collaborators/deleteCollaborator.js
+++
@@ -1,8 +0,0 @@
-const deleteCollaborator = (_req, res, next) => {
-    try {
-        res.send({})
-    } catch (error) {
-        next(error)
-    }
-}
-export default deleteCollaborator
--- backend/src/routes/activitiesRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { joinPartnerActivity, joinFreeActivity } from '../controllers/index.js'
-const router = express.Router()
-
-//Ruta para actividades:
-router.post('/join-partner-activity', joinPartnerActivity)
-router.post('/join-non-partner-activity', joinFreeActivity)
-
-export default router
