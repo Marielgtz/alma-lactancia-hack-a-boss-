2b8075e61c1a2b978038896a59456ea53a1356b0
Alba Fresnillo Arévalo <fresnilloarevaloalba@gmail.com>
2024-08-18 16:52:19 +0200

    Agrega backend al .gitignore y deja de rastrearla

--- backend/src/routes/captchaRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { generateCaptcha, validateCaptcha } from '../controllers/index.js'
-const router = express.Router()
-
-router.get('/generate-captcha', generateCaptcha)
-router.post('/validate-captcha', validateCaptcha)
-
-export default router
