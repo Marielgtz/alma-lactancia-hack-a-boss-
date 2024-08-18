a48a12e8472112f03c972e7030b7339ca141dfb6
Alba Fresnillo Arévalo <144828195+AlbaFresnillo@users.noreply.github.com>
2024-08-18 16:51:06 +0200

    Delete backend directory

--- backend/src/controllers/googleSignIn/checkSession.js
+++
@@ -1,9 +0,0 @@
-const checkSession = (req, res) => {
-    if (req.session.user) {
-        res.json({ user: req.session.user })
-    } else {
-        res.status(401).json({ message: 'No hay sesión' })
-    }
-}
-
-export default checkSession
--- backend/src/routes/adminRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { googleSignIn, checkSession, logout } from '../controllers/index.js'
-const router = express.Router()
-
-router.get('/auth/callback', googleSignIn)
-router.get('/check-session', checkSession)
-router.post('/logout', logout)
-
-export default router
