8895e1a9c2b67af8052612424a5434aa20a77d66
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-08-21 12:52:11 +0200

    Añadir componente login

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Login = () => {
+    const backURL = import.meta.env.VITE_API_URL
+    const clientId = import.meta.env.VITE_CLIENT_ID
+    const googleURL = import.meta.env.VITE_GOOGLE_URL
+
+    const handleLogin = () => {
+        window.location.href = `${googleURL}?client_id=${clientId}&redirect_uri=${backURL}/auth/callback&response_type=code&scope=email%20profile`
+    }
+
+    return (
+        <div>
+            <h1>Inicia sesión con Google</h1>
+            <button onClick={handleLogin}>Iniciar sesión</button>
+        </div>
+    )
+}
+
+export default Login
