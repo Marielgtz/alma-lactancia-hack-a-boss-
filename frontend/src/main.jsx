5d49ae2bc2b4f2b5769e6b4d47253323ac4d9213
Alba Fresnillo Arévalo <fresnilloarevaloalba@gmail.com>
2024-08-30 20:22:54 +0200

    logica y diseño activities

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,48 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-
-function ScrollToTopButton() {
-  useEffect(() => {
-    const $btnUp = document.getElementById("up");
-
-    console.log('$btnUp:', $btnUp);
-
-    if (!$btnUp) {
-      console.error('Button element not found!');
-      return;
-    }
-
-    const handleScroll = () => {
-      const y = window.scrollY;
-      if (y >= 100) {
-        $btnUp.classList.remove("hide");
-        $btnUp.classList.add("active");
-      } else {
-        $btnUp.classList.add("hide");
-        $btnUp.classList.remove("active");
-      }
-    };
-
-    const handleClick = () => {
-      window.scrollTo({
-        behavior: "smooth",
-        top: 0,
-      });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    $btnUp.addEventListener("click", handleClick);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      $btnUp.removeEventListener("click", handleClick);
-    };
-  }, []);
-
-  return null;
-}
+import ScrollToTopButton from "./components/ButtonUp";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
