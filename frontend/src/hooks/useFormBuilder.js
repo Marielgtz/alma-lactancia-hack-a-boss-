838cfc7024a5f19ef445f5a45a92ac7f8fbeb021
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-09-04 17:25:04 +0200

    Sistema de gestios de formularios

---
+++ frontend/src/hooks/useFormBuilder.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import { useForm, useFieldArray } from 'react-hook-form'
+import { v4 as uuidv4 } from 'uuid'
+
+const useFormBuilder = (setForms) => {
+    let formId
+    const { register, handleSubmit, control, reset } = useForm()
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: 'fields',
+    })
+
+    const onSubmit = (data) => {
+        formId = uuidv4()
+        const jsonData = {
+            formId,
+            formName: data.formName,
+            fields: data.fields,
+        }
+        const saveForm = import.meta.env.VITE_API_URL + '/create-form'
+
+        fetch(saveForm, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(jsonData),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                }
+            })
+            .then((dataFromBack) => {
+                console.log(dataFromBack.message)
+
+                setForms((prevData) => {
+                    return { ...prevData, ...dataFromBack.form }
+                })
+            })
+
+        reset()
+    }
+    return {
+        onSubmit,
+        register,
+        handleSubmit,
+        control,
+        reset,
+        fields,
+        append,
+        remove,
+        formId,
+    }
+}
+export default useFormBuilder
