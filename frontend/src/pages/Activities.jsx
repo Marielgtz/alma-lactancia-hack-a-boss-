495c04209f1e399f0e072f23f6113f103a1c9fc2
Alba Fresnillo Arévalo <fresnilloarevaloalba@gmail.com>
2024-09-16 18:41:53 +0200

    ajustes en varias paginas y componentes

--- frontend/src/pages/Activities.jsx
+++ frontend/src/pages/Activities.jsx
@@ -54,124 +54,13 @@ const Activities = () => {
 
   const handleEnrollClick = (activity) => {
     setSelectedActivity(activity); // Almacena la actividad seleccionada
-    setShowForm(true); // Muestra el formulario
-  };
-
-  const handleChange = (e) => {
-    const { name, type, value, checked } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: type === "checkbox" ? checked : value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Formulario enviado:", formData);
-    setShowForm(false); // Oculta el formulario después de enviar
-    setFormData({
-      name: "",
-      email: "",
-      phone: "",
-      isMember: "",
-      acceptPrivacyPolicy: false,
-    }); // Reinicia el formulario
+    navigate("/formulario-inscripcion", { state: { activity } });
   };
 
   return (
     <div className="activity-page">
       <Header />
       <main className="activity-main">
-        {/* Formulario de inscripción */}
-        {showForm && selectedActivity && (
-          <div className="registration-form">
-            <small className="registration-title">Alma Lactancia</small>
-            <h1 className="registration-h1">
-              Rellena el formulario para inscribirte a esta actividad
-            </h1>
-            <h2 className="registration-summary">Obradoiro: {selectedActivity.summary}</h2>
-            <p className="registration-date">{selectedActivity.exactDate}</p>
-            <h2 className="registration-h2">FORMULARIO DE INSCRIPCIÓN</h2>
-            <form onSubmit={handleSubmit}>
-              <label className="registration-label">
-                Nombre y apellidos:
-                <input
-                  type="text"
-                  className="registration-input"
-                  name="name"
-                  placeholder="Nombre y apellidos"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                />
-              </label>
-              <label className="registration-label">
-                Correo electrónico:
-                <input
-                  type="email"
-                  className="registration-input"
-                  name="email"
-                  placeholder="Correo electrónico"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-              </label>
-              <label className="registration-label">
-                Teléfono:
-                <input
-                  type="tel"
-                  className="registration-input"
-                  name="phone"
-                  placeholder="Teléfono"
-                  value={formData.phone}
-                  onChange={handleChange}
-                  required
-                />
-              </label>
-              <label className="registration-label">
-                ¿Eres soci@?
-                <div>
-                  <label className="registration-yes">
-                    <input
-                      type="radio"
-                      className="registration-check"
-                      name="isMember"
-                      value="yes"
-                      checked={formData.isMember === "yes"}
-                      onChange={handleChange}
-                      required
-                    />
-                    Sí
-                  </label>
-                  <label className="registration-no">
-                    <input
-                      type="radio"
-                      className="registration-check"
-                      name="isMember"
-                      value="no"
-                      checked={formData.isMember === "no"}
-                      onChange={handleChange}
-                      required
-                    />
-                    No
-                  </label>
-                </div>
-              </label>
-              <label className="registration-label">
-                <input
-                  type="checkbox"
-                  name="acceptPrivacyPolicy"
-                  checked={formData.acceptPrivacyPolicy}
-                  onChange={handleChange}
-                  required
-                />
-                He leído y acepto la política de privacidad
-              </label>
-              <button className="registration-button" type="submit">Inscribirse</button>
-            </form>
-          </div>
-        )}
         <div className="activity-header">
           <p className="activity-text">Alma Lactancia</p>
           <h1 className="activity-title">Próximas actividades</h1>
