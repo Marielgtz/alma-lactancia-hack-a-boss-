dd3b4cc3df2b0c225730dbd5fe98dcb851b503ac
Alba Fresnillo Arévalo <fresnilloarevaloalba@gmail.com>
2024-09-17 18:07:32 +0200

    correcciones necesarias para que pinte el formulario de inscripcion a actividad

--- frontend/src/pages/FormActivityPage.jsx
+++ frontend/src/pages/FormActivityPage.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import FormDisplay from "../components/FormDisplay";
 import "./FormActivityPage.css";
 
-const FormActivityPage = () => {
-  const location = useLocation();
-  const selectedActivity = location.state?.activity; // Obtiene la actividad seleccionada
-  const event = location.state?.event; // Obtiene el evento seleccionado
-  const publishedForm = location.state?.publishedForm; // Obtiene publishedForm si está disponible
-
+const FormActivityPage = ({ publishedForm, setPublishedForm }) => {
   return (
     <div className="register-page">
       <Header />
       <main className="register-main">
-        <FormDisplay publishedForm={publishedForm} selectedActivity={selectedActivity} /> {/* Pasa la actividad seleccionada al formulario */}
+        {/* Pasamos publishedForm y setPublishedForm como props */}
+        <FormDisplay
+          publishedForm={publishedForm}
+          setPublishedForm={setPublishedForm}
+          jsonNumber={1}
+        />
       </main>
       <Footer />
     </div>
