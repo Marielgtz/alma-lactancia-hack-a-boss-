b9b61d525533c09a5424e972c7c30234d451ddda
Helena Artola <116822570+helenartola@users.noreply.github.com>
2024-09-19 19:22:24 +0200

    Cambios en footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -16,8 +16,8 @@ const Footer = () => {
     window.location.href = `${googleURL}?client_id=${clientId}&redirect_uri=${backURL}/auth/callback&response_type=code&scope=email%20profile`;
   };
 
-  const instagramLink = generalSettings?.linkInstagram || '#';
-  const facebookLink = generalSettings?.linkFacebok || '#';
+  const instagramLink = generalSettings?.linkInstagram || "#";
+  const facebookLink = generalSettings?.linkFacebok || "#";
 
   return (
     <footer className="App-footer">
@@ -45,42 +45,14 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="footer-column">
-          <h3>Actividades</h3>
-          <ul>
-            <li>
-              <a href="/">Actividad 1</a>
-            </li>
-            <li>
-              <a href="/">Actividad 2</a>
-            </li>
-            <li>
-              <a href="/">Actividad 3</a>
-            </li>
-            <li>
-              <a href="/">Actividad 4</a>
-            </li>
-            <li>
-              <a href="/">Actividad 5</a>
-            </li>
-            {/* Añade más actividades según sea necesario */}
-          </ul>
-        </div>
+
         <div className="footer-column">
           <h3>Síguenos :)</h3>
           <div className="social-media">
-            <a
-              href={instagramLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href={instagramLink} target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faInstagram} size="2x" />
             </a>
-            <a
-              href={facebookLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href={facebookLink} target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faFacebookF} size="2x" />
             </a>
           </div>
@@ -88,7 +60,9 @@ const Footer = () => {
       </div>
       <div className="admin">
         <div className="admin-links">
-          <a href="#" onClick={handleLogin} className="button-login">Acceso administración</a>
+          <a href="#" onClick={handleLogin} className="button-login">
+            Acceso administración
+          </a>
           <a href="/politica-de-privacidad">Política de privacidad</a>
           <a href="/politica-de-cookies">Política de cookies</a>
           <a href="/aviso-legal">Aviso legal</a>
