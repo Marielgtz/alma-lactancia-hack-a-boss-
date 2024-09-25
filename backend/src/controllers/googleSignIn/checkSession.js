b9f0f100db0c4e2a5b69ffe69b316c1b450de237
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-09-25 13:20:52 +0200

    Cambiar mensaje de checkSession

--- backend/src/controllers/googleSignIn/checkSession.js
+++ backend/src/controllers/googleSignIn/checkSession.js
@@ -1,6 +1,6 @@
 const checkSession = (req, res) => {
     if (req.session.user) {
-        res.json({ user: req.session.user })
+        res.json({ user: req.session.user, message: 'Hay sesión activa' })
     } else {
         res.status(401).json({ message: 'No hay sesión' })
     }
