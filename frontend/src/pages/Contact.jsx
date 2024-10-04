c56e13aa6a957c23c0fdff460573bfb38c91fb13
Roberto Panella <tito874@hotmail.com>
2024-10-04 06:06:45 +0200

    arreglo de contactos

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -17,8 +17,6 @@ const Contact = () => {
         comments: '',
     })
 
-    // const navigate = useNavigate()
-
     const handleChange = (e) => {
         const { name, value } = e.target
         setFormData({
@@ -57,138 +55,137 @@ const Contact = () => {
                 comments: '',
             })
 
-            // // Redirigir a la página de confirmación
-            // navigate('/confirmacion')
         } catch (error) {
             console.error(error)
             alert(error)
         }
     }
 
-  };
+    const instagramLink = generalSettings?.linkInstagram || ""
+    const facebookLink = generalSettings?.linkFacebook || ""
+    const email = generalSettings?.email || ""
 
-  const instagramLink = generalSettings?.linkInstagram || "";
-  const facebookLink = generalSettings?.linkFacebook || "";
-  const email = generalSettings?.email || "";
+    return (
+        <div className="contact-page">
+            <Header />
+            <main className="contact-main">
+                <h1 className="contact-title">Contacto</h1>
+                <div className="contact-content">
+                    <form id="contact">
+                        <div className="name-fields">
+                            <div className="name-field">
+                                <label htmlFor="name" className="etiquetas-contacto">
+                                    Nombre completo
+                                    <span className="required">*</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                    className="inputs-contacto"
+                                    required
+                                />
+                            </div>
 
-  return (
-    <div className="contact-page">
-      <Header />
-      <main className="contact-main">
-        <h1 className="contact-title">Contacto</h1>
-        <div className="contact-content">
-          <form id="contact">
-            <div className="name-fields">
-              <div className="name-field">
-                <label htmlFor="name" className="etiquetas-contacto">
-                  Nombre completo
-                  <span className="required">*</span>
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  className="inputs-contacto"
-                  required
-                />
-              </div>
+                            <div className="name-field">
+                                <label htmlFor="surname" className="etiquetas-contacto">
+                                    Apellido completo
+                                    <span className="required">*</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    id="surname"
+                                    name="surname"
+                                    value={formData.surname}
+                                    onChange={handleChange}
+                                    className="inputs-contacto"
+                                    required
+                                />
+                            </div>
+                        </div>
 
-              <div className="name-field">
-                <label htmlFor="surname" className="etiquetas-contacto">
-                  Apellido completo
-                  <span className="required">*</span>
-                </label>
-                <input
-                  type="text"
-                  id="surname"
-                  name="surname"
-                  value={formData.surname}
-                  onChange={handleChange}
-                  className="inputs-contacto"
-                  required
-                />
-              </div>
-            </div>
+                        <label htmlFor="email" className="etiquetas-contacto">
+                            Correo electrónico
+                            <span className="required">*</span>
+                        </label>
+                        <input
+                            type="email"
+                            className="inputs-contacto"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
 
-            <label htmlFor="email" className="etiquetas-contacto">
-              Correo electrónico
-              <span className="required">*</span>
-            </label>
-            <input
-              type="email"
-              className="inputs-contacto"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
+                        <label htmlFor="subject" className="etiquetas-contacto">
+                            Asunto<span className="required">*</span>
+                        </label>
+                        <input
+                            type="text"
+                            className="inputs-contacto"
+                            id="subject"
+                            name="subject"
+                            value={formData.subject}
+                            onChange={handleChange}
+                            required
+                        />
 
-            <label htmlFor="subject" className="etiquetas-contacto">
-              Asunto<span className="required">*</span>
-            </label>
-            <input
-              type="text"
-              className="inputs-contacto"
-              id="subject"
-              name="subject"
-              value={formData.subject}
-              onChange={handleChange}
-              required
-            />
+                        <label htmlFor="comments" className="etiquetas-contacto">
+                            Mensaje
+                        </label>
+                        <textarea
+                            id="comments"
+                            name="comments"
+                            value={formData.comments}
+                            onChange={handleChange}
+                            className="area-texto-contacto"
+                        ></textarea>
 
-            <label htmlFor="comments" className="etiquetas-contacto">
-              Mensaje
-            </label>
-            <textarea
-              id="comments"
-              name="comments"
-              value={formData.comments}
-              onChange={handleChange}
-              className="area-texto-contacto"
-            ></textarea>
+                        <Captcha
+                            handleSubmit={handleSubmit}
+                            captchaInputClassName={"captcha-input"}
+                            buttonClassName={"contact-button"}
+                        />
+                    </form>
 
-            <Captcha
-              handleSubmit={handleSubmit}
-              captchaInputClassName={"captcha-input"}
-              buttonClassName={"contact-button"}
-            />
-          </form>
-
-          <div className="contact-info">
-            <p>
-              También podéis contactar con la Asociación ALMA a través de
-              cualquiera de los siguientes medios:
-            </p>
-            <p>
-              <a href={email} className="contact-info-icon">
-                <i className="fas fa-envelope"></i> {email}
-              </a>
-            </p>
-            <p>
-              <a
-                href={instagramLink}
-                className="contact-info-icon"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-instagram"></i> {instagramLink}
-              </a>
-            </p>
-            <p>
-              <a
-                href={facebookLink}
-                className="contact-info-icon"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-facebook"></i> {facebookLink}
-              </a>
-            </p>
-            <img src={silueta} />
-          </div>
+                    <div className="contact-info">
+                        <p>
+                            También podéis contactar con la Asociación ALMA a través de
+                            cualquiera de los siguientes medios:
+                        </p>
+                        <p>
+                            <a href={`mailto:${email}`} className="contact-info-icon">
+                                <i className="fas fa-envelope"></i> {email}
+                            </a>
+                        </p>
+                        <p>
+                            <a
+                                href={instagramLink}
+                                className="contact-info-icon"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <i className="fab fa-instagram"></i> {instagramLink}
+                            </a>
+                        </p>
+                        <p>
+                            <a
+                                href={facebookLink}
+                                className="contact-info-icon"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <i className="fab fa-facebook"></i> {facebookLink}
+                            </a>
+                        </p>
+                        <img src={silueta} alt="Ilustración de lactancia" />
+                    </div>
+                </div>
+            </main>
+            <Footer />
         </div>
     )
 }
