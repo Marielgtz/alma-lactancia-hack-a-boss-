d74f1321d791d0cb0236d01095e3c6cc6cb659c6
Helena Artola <116822570+helenartola@users.noreply.github.com>
2024-10-07 21:45:56 +0200

    Hemos modificado css de experiencias reales del dashboard

--- frontend/src/components/private/Experiences.jsx
+++ frontend/src/components/private/Experiences.jsx
@@ -4,8 +4,8 @@ import "./AdminHome.css";
 import { useEffect } from "react";
 
 // URL de la imagen proporcionada (icono pecho)
-const DEFAULT_IMAGE_URL = 'https://res.cloudinary.com/dqhemn1nv/image/upload/v1728065521/59e10e0a-c67b-46bc-a663-2f66f7316077.png'    
-
+const DEFAULT_IMAGE_URL =
+  "https://res.cloudinary.com/dqhemn1nv/image/upload/v1728065521/59e10e0a-c67b-46bc-a663-2f66f7316077.png";
 
 const Experiences = ({
   homeData,
@@ -35,16 +35,18 @@ const Experiences = ({
 
   useEffect(() => {
     console.log(selectedExperience);
-    
-  },[selectedExperience])
+  }, [selectedExperience]);
 
   return (
     <div
       className={`section ${visibleSection === "experiences" ? "visible" : ""}`}
     >
       <h2>Editar experiencias reales</h2>
-
-      <label htmlFor="experienceText">Añade una descripción:</label>
+      <p className="texto-descriptivo-experiencias">
+        Para poder compartir una experiencia en la página web, primero debes
+        crear una:
+      </p>
+      <label htmlFor="experienceText">1. Añade una descripción:</label>
       <textarea
         id="experienceText"
         name="text"
@@ -57,7 +59,7 @@ const Experiences = ({
       </p>
 
       <div>
-        <label htmlFor="experienceImage">Añade una imagen:</label>
+        <label htmlFor="experienceImage">2. Añade una imagen:</label>
         <button
           className="experience-buttons"
           onClick={() => {
@@ -81,39 +83,59 @@ const Experiences = ({
       <button className="admin-btn-exp" onClick={handleAddExperience}>
         <i className="fas fa-plus"></i> Añadir experiencia
       </button>
+      <p className="texto-descriptivo-experiencias">
+        Una vez añadida la experiencia, selecciona las experiencias del listado
+        que desees y haz clic en publicar para compartirlas{" "}
+        <i className="fas fa-arrow-down"></i>
+      </p>
       <div className="fondo-lista-experiencias">
-        <h3>Listado de experiencias actualmente publicadas:</h3>
+        <h3>Listado de experiencias actualmente creadas</h3>
+
+        <p>
+          (Haz clic en la imagen para editar cualquier experiencia publicada)
+        </p>
         <ol className="list-exp">
           {homeData.experiences && homeData.experiences.length > 0 ? (
             homeData.experiences.map((experience) =>
               experience ? (
                 <li key={experience.id}>
-                  <img
-                    className="texto-experiencias-dashboard"
-                    src={experience.image !== 'Sin imagen'
-                      ? experience.image
-                      : DEFAULT_IMAGE_URL
-                    }
-                    alt={experience.text || "Experience Image"}
-                    onClick={() => {                      
-                      setSelectedExperience(experience);
-                      setModalOpen(true);
-                    }}
-                    style={{ cursor: "pointer" }}
-                  />
-                  <p className="texto-experiencias-dashboard">
-                    {experience.text}
-                  </p>
+                  <div className="contenedor-experiencia">
+                    <img
+                      className="imagen-experiencias-dashboard"
+                      src={
+                        experience.image !== "Sin imagen"
+                          ? experience.image
+                          : DEFAULT_IMAGE_URL
+                      }
+                      alt={experience.text || "Experience Image"}
+                      onClick={() => {
+                        setSelectedExperience(experience);
+                        setModalOpen(true);
+                      }}
+                      style={{ cursor: "pointer" }}
+                    />
+                    <p className="texto-experiencias-dashboard">
+                      {experience.text}
+                    </p>
+                  </div>
                   {/* Checkbox para seleccionar la experiencia */}
-                  <input
-                    type="checkbox"
-                    checked={checkedExperiences.includes(experience.id)}
-                    onChange={() => handleCheckboxChange(experience.id)}
-                    disabled={
-                      !checkedExperiences.includes(experience.id) &&
-                      checkedExperiences.length >= 4
-                    }
-                  />
+                  <div className="contenedor-checkbox-experiencias">
+                    <input
+                      className="checkbox-experiencias-publicadas"
+                      type="checkbox"
+                      id={`checkbox-${experience.id}`}
+                      checked={checkedExperiences.includes(experience.id)}
+                      onChange={() => handleCheckboxChange(experience.id)}
+                      disabled={
+                        !checkedExperiences.includes(experience.id) &&
+                        checkedExperiences.length >= 4
+                      }
+                    />
+                    <label htmlFor={`checkbox-${experience.id}`}>
+                      {" "}
+                      <i className="fas fa-check"></i>{" "}
+                    </label>
+                  </div>
                 </li>
               ) : null
             )
@@ -123,7 +145,10 @@ const Experiences = ({
             </p>
           )}
         </ol>
-        <button onClick={handleSaveSelection}>
+        <button
+          className="boton-publicar-experiencias"
+          onClick={handleSaveSelection}
+        >
           Publicar experiencias seleccionadas
         </button>
       </div>
