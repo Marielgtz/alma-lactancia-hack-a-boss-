84d57ba5a22ac7fcc553c3257bab2c775328ee52
Helena Artola <116822570+helenartola@users.noreply.github.com>
2024-10-14 22:12:35 +0200

    Hemos corregido el color de los p de admin formularios

--- frontend/src/components/private/CreadorFormularios.jsx
+++ frontend/src/components/private/CreadorFormularios.jsx
@@ -26,14 +26,20 @@ const CreadorFormularios = ({ publishedForm, setPublishedForm }) => {
         />
       ) : (
         <>
-            <div className="contenedor-pasos">
-              <p>En esta sección podéis gestionar vuestros formularios. Desde aquí se puede PUBLICAR, CREAR y MODIFICAR los formularios.</p>
+          <div className="contenedor-pasos">
+            <p>
+              En esta sección podéis gestionar vuestros formularios. Desde aquí
+              se puede PUBLICAR, CREAR y MODIFICAR los formularios.
               <br></br>
-              <p>A continuación, tenéis la lista con los eventos existentes, clicad en cada uno para ver las opciones disponibles. Si deseáis crear uno nuevo, encontraréis el creador de formularios al final de la página.</p>
-            </div>
-            <h3 className="titulo-descriptivo-accion">
-             Lista de formularios <i class="fas fa-arrow-down"></i>
-            </h3>
+              <br></br>A continuación, tenéis la lista con los eventos
+              existentes, clicad en cada uno para ver las opciones disponibles.
+              Si deseáis crear uno nuevo, encontraréis el creador de formularios
+              al final de la página.
+            </p>
+          </div>
+          <h3 className="titulo-descriptivo-accion">
+            Lista de formularios <i class="fas fa-arrow-down"></i>
+          </h3>
           <FormDropdown
             forms={forms}
             setForms={setForms}
@@ -44,17 +50,21 @@ const CreadorFormularios = ({ publishedForm, setPublishedForm }) => {
             setSelectedForm={setSelectedForm}
           />
           <div>
-            
             <h3 className="titulo-descriptivo-accion">
               Creador de formularios <i class="fas fa-arrow-down"></i>
             </h3>
             <div className="contenedor-pasos">
-              <p>Los formularios que creéis aquí se añadirán a la lista anterior. </p>
-              <br />
-              <p>CAMPOS: Añadid tantos como datos necesitéis recoger de cada participante. Si queréis recoger teléfono o email, usad el campo específico para que al usarlo se verifique que os envían números y correos válidos.</p>
-              <br />
-              <p>Podéis usar el tipo "texto" para todo lo que no encaje con las categorías.</p>
-
+              <p>
+                Los formularios que creéis aquí se añadirán a la lista anterior.{" "}
+                <br /> <br />
+                CAMPOS: Añadid tantos como datos necesitéis recoger de cada
+                participante. Si queréis recoger teléfono o email, usad el campo
+                específico para que al usarlo se verifique que os envían números
+                y correos válidos.
+                <br /> <br />
+                Podéis usar el tipo "texto" para todo lo que no encaje con las
+                categorías.
+              </p>
             </div>
             <FormBuilder setForms={setForms} />
           </div>
